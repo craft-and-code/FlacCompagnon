56540b6a9163d6c58f1c3a63f8cd3e610fa8dca0
craft-and-code <craft-and-code@users.noreply.github.com>
2026-08-11 15:32:21 +0000

    deploy: fa140e9e9e519abc4efb69810691f45e1b6db3a6

---
+++ doc/flaccompagnon_core/tags/addable/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["addable_tags"],"struct":["AddableTag"]};
--- doc/flaccompagnon_core/tags/sidebar-items.js
+++ doc/flaccompagnon_core/tags/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["FieldEdit","TagError"],"fn":["read_tags","write_tags"],"mod":["cover"],"struct":["TagEdits","TagSet"]};
+window.SIDEBAR_ITEMS = {"enum":["FieldEdit","TagError"],"fn":["read_tags","write_tags"],"mod":["addable","cover"],"struct":["TagEdits","TagSet"]};
