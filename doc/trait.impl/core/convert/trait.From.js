36ff46c2d3ad6dbe4a52e3fd981c5443e2833c33
craft-and-code <craft-and-code@users.noreply.github.com>
2026-09-03 17:06:38 +0000

    deploy: 4f5164a44ad56a472ccd1e3e68d72d7466e0ff48

--- doc/trait.impl/core/convert/trait.From.js
+++ doc/trait.impl/core/convert/trait.From.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["flaccompagnon_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/1.98.0/core/io/error/struct.Error.html\" title=\"struct core::io::error::Error\">Error</a>&gt; for <a class=\"enum\" href=\"flaccompagnon_core/types/enum.AnalysisError.html\" title=\"enum flaccompagnon_core::types::AnalysisError\">AnalysisError</a>",0]]]]);
+    const implementors = Object.fromEntries([["flaccompagnon_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/1.98.1/core/io/error/struct.Error.html\" title=\"struct core::io::error::Error\">Error</a>&gt; for <a class=\"enum\" href=\"flaccompagnon_core/types/enum.AnalysisError.html\" title=\"enum flaccompagnon_core::types::AnalysisError\">AnalysisError</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
